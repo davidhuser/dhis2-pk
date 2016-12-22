045ad894a5c324ff664b8d454f7fab528f774285
David Huser <davidhuser303@bitbucket.com>
2016-12-22 15:20:24 +0100

    common printing errors handled

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dhis2-pocket-knife',
-    version='0.2.20',
+    version='0.2.21',
     description='Command-line tools for interacting with DHIS2 API in bulk',
     author='David Huser',
     author_email='dhuser@baosystems.com',
