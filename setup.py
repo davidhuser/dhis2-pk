0d169f7ae0ec25baaf05388310729a15cbe459d7
David Huser <davidhuser303@bitbucket.com>
2017-01-25 14:25:51 +0100

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dhis2-pocket-knife',
-    version='0.2.25',
+    version='0.2.26',
     description='Command-line tools for interacting with DHIS2 API in bulk',
     author='David Huser',
     author_email='dhuser@baosystems.com',
